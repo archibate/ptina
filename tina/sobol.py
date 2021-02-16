1a3603a1e17a54bc9853f64f75af0e07ca29473f
archibate <1931127624@qq.com>
2021-02-16 13:08:24 +0800

    add encoding assets

--- tina/sobol.py
+++ tina/sobol.py
@@ -1,58 +1,8 @@
-import numpy as np
-
-
-# https://web.maths.unsw.edu.au/~fkuo/sobol/
-def NumpySobol(N, D):
-    print(f'[TinaSobol] reading data from file...')
-    f = open('/home/bate/Downloads/new-joe-kuo-6.21201')
-    f.readline()
-
-    print(f'[TinaSobol] initializing with N={N}, D={D}')
-
-    L = int(np.ceil(np.log2(N)))
-
-    def C(i):
-        bits = 1
-        value = i
-        while value & 1:
-            value >>= 1
-            bits += 1
-        return bits
-
-    V = np.full((L + 1, D), 0)
-
-    for j in range(D):
-        if j != 0:
-            _, s, a, M = f.readline().split(maxsplit=3)
-            s, a = int(s), int(a)
-            m = np.full(s + 1, 0)
-            for i, M in enumerate(map(int, M.split())):
-                m[i + 1] = M
-        else:
-            m = np.full(L + 1, 1)
-            s = L
-
-        if L <= s:
-            for i in range(L + 1):
-                V[i, j] = m[i] << (32 - i)
-        else:
-            for i in range(s + 1):
-                V[i, j] = m[i] << (32 - i)
-            for i in range(s + 1, L + 1):
-                V[i, j] = V[i - s, j] ^ (V[i - s, j] >> s)
-                for k in range(1, s):
-                    V[i, j] ^= ((a >> (s - 1 - k)) & 1) * V[i - k, j]
-
-    print(f'[TinaSobol] start generating sequence')
-
-    X = np.full(D, 0)
-    for i in range(N):
-        P = X / 2**32
-        X = X ^ V[C(i), :]
-        yield P
-
-
-import taichi as ti
+from tina.common import *
+try:
+    from pysobol import Sobol as NumpySobol
+except ImportError:
+    please_install('pysobol')
 
 
 @ti.func
@@ -174,3 +124,6 @@ if __name__ == '__main__':
         gui1.show()
         gui2.show()
         gui3.show()
+
+
+__all__ = ['wanghash', 'wanghash2', 'wanghash3', 'TaichiSobol']
