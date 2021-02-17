2a97096f2be2406c0779eb4489f2ab710323152e
archibate <1931127624@qq.com>
2021-02-17 14:44:38 +0800

    fix light for point

--- tina/light.py
+++ tina/light.py
@@ -4,43 +4,63 @@ from tina.geometries import *
 
 @ti.data_oriented
 class LightPool(metaclass=Singleton):
+    TYPES = {'POINT': 1, 'AREA': 2}
+
     def __init__(self, count=2**6):
         self.color = ti.Vector.field(3, float, count)
         self.pos = ti.Vector.field(3, float, count)
-        self.radius = ti.field(float, count)
+        self.size = ti.field(float, count)
+        self.type = ti.field(int, count)
         self.count = ti.field(int, ())
 
     def clear(self):
         self.count[None] = 0
 
-    def add(self, color, pos, radius):
+    def add(self, world, color, size, type):
         i = self.count[None]
-        self.color[i] = color
-        self.pos[i] = pos
-        self.radius[i] = radius
+
+        pos = world @ np.array([0, 0, 0, 1])
+        pos = pos[:3] / pos[3]
+
+        self.type[i] = self.TYPES[type]
+        self.color[i] = color.tolist()
+        self.pos[i] = pos.tolist()
+        self.size[i] = size
+
         self.count[None] = i + 1
         return i
 
     @ti.func
     def hit(self, ray):
         ret = namespace(dis=0.0, pdf=0.0, color=V3(0.0))
+
         for i in range(self.count[None]):
-            t = Sphere(self.pos[i], self.radius[i]**2).intersect(ray)
+            type = self.type[i]
+            color = self.color[i]
+            pos = self.pos[i]
+            size = self.size[i]
+
+            t = 0.0
+            if type == self.TYPES['POINT']:
+                t = Sphere(pos, size**2).intersect(ray)
+            elif type == self.TYPES['AREA']:
+                t = 2.33
+
             if t != 0:
                 ret.dis = t
-                ret.pdf = ret.dis**2 / (ti.pi * self.radius[i]**2)
-                ret.color = self.color[i]
+                ret.pdf = ret.dis**2 / (ti.pi * size**2)
+                ret.color = color
                 break
         return ret
 
     @ti.func
     def sample(self, pos, samp):
         i = clamp(ifloor(samp.z * self.count[None]), 0, self.count[None])
-        litpos = self.pos[i] + self.radius[i] * spherical(samp.x, samp.y)
+        litpos = self.pos[i] + self.size[i] * spherical(samp.x, samp.y)
         toli = litpos - pos
 
         dis = toli.norm()
         dir = toli / dis
-        pdf = dis**2 / (ti.pi * self.radius[i]**2)
+        pdf = dis**2 / (ti.pi * self.size[i]**2)
         color = self.color[i] / pdf
         return namespace(dis=dis, dir=dir, pdf=pdf, color=color)
