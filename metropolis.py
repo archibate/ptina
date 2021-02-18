cceabbce31a6a58641b480347a6d624d6775571e
archibate <1931127624@qq.com>
2021-02-18 23:08:34 +0800

    did work metropolis

--- metropolis.py
+++ metropolis.py
@@ -1,8 +1,19 @@
 import numpy as np
+import taichi as ti
 
 
 def evaluate(X):
     return (X[0] - 0.5)**2 + (X[1] - 0.34)**2
+
+
+M = 128
+film = np.zeros((M, M))
+count = np.zeros((M, M)) + 1e-10
+
+def splat(X, L):
+    I = tuple(np.int32(np.floor(X * M)))
+    film[I] += L
+    count[I] += 1
 
 
 kT = 10
@@ -10,26 +21,27 @@ kT = 10
 N = 2
 
 X = np.random.rand(N)
-E = evaluate(X)
+L = evaluate(X)
 
-while kT > 0.0005:
+
+for i in range(M * M):
     X_old = X
-    E_old = E
+    L_old = L
     dX = np.random.normal(0, 0.1, N)
-    X_new = np.clip(X + dX, 0, 1)
-    E_new = evaluate(X_new)
+    X_new = (X + dX) % 1
+    L_new = evaluate(X_new)
 
-    accept = False
-    if E_new <= E_old:
-        accept = True
-    elif np.exp(-(E_new - E_old) / kT) > np.random.rand():
-        accept = True
-    else:
-        accept = False
+    accept = min(1, L_new / L_old)
+    if accept > 0:
+        splat(X_new, accept)
+    splat(X_old, 1 - accept)
 
-    if accept:
+    if accept > np.random.rand():
+        L = L_new
         X = X_new
+    else:
+        L = L_old
+        X = X_old
 
-    kT *= np.exp(-0.002)
 
-    print(X, E)
+ti.imshow(ti.imresize(film / count, 512))
