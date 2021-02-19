154906a0524f692ef9102b1db836efc77491cdae
archibate <1931127624@qq.com>
2021-02-19 13:57:52 +0800

    walking mlt

--- metropolis.py
+++ metropolis.py
@@ -6,53 +6,61 @@ def trace(X):
     return (X[0] - 0.5)**2 + (X[1] - 0.34)**2
 
 
-M = 64
-film = ti.field(float, (M, M))
-count = ti.field(float, (M, M))
+nres = V(64, 64)
+film = ti.field(float, nres)
+count = ti.field(float, nres)
 
 
 @ti.func
 def splat(X, L):
-    I = ifloor(X * M)
+    I = clamp(ifloor(X * nres), 0, nres - 1)
     film[I] += L
     count[I] += 1
 
 
 LSP = 0.1
 
+nchains = 1024
+ndims = 2
+
+
+Xs = ti.Vector.field(ndims, float, nchains)
+Ls = ti.field(float, nchains)
+
+
 @ti.kernel
 def render():
-    for _ in range(16):
-        X = V2(0.0)
-        L = 0.0
+    for i in range(nchains):
+        X_old = Xs[i]
+        L_old = Ls[i]
 
-        for i in range(M * M):
-            X_old = X
-            L_old = L
+        X_new = X_old
+        if i == 0 or ti.random() < LSP:
+            X_new = random2(ti)
+        else:
+            dX = 0.1 * normaldist(random2(ti))
+            X_new = (X_old + dX) % 1
 
-            X_new = X
-            if i == 0 or ti.random() < LSP:
-                X_new = random2(ti)
-            else:
-                dX = 0.1 * normaldist(random2(ti))
-                X_new = (X + dX) % 1
+        L_new = trace(X_new)
 
-            L_new = trace(X_new)
+        AL_new = Vavg(L_new) + 1e-10
+        AL_old = Vavg(L_old) + 1e-10
+        accept = min(1, AL_new / AL_old)
+        if accept > 0:
+            splat(X_new, accept * L_new / AL_new)
+        splat(X_old, 1 - accept * L_old / AL_old)
 
-            AL_new = Vavg(L_new) + 1e-10
-            AL_old = Vavg(L_old) + 1e-10
-            accept = min(1, AL_new / AL_old)
-            if accept > 0:
-                splat(X_new, accept * L_new / AL_new)
-            splat(X_old, 1 - accept * L_old / AL_old)
+        if accept > ti.random():
+            L_old = L_new
+            X_old = X_new
 
-            if accept > ti.random():
-                L = L_new
-                X = X_new
-            else:
-                L = L_old
-                X = X_old
+        Xs[i] = X_old
+        Ls[i] = L_old
 
 
-render()
-ti.imshow(ti.imresize(film.to_numpy() / (count.to_numpy() + 1e-10), 512))
+gui = ti.GUI()
+
+while gui.running and not gui.get_event(gui.ESCAPE):
+    render()
+    gui.set_image(ti.imresize(film.to_numpy() / (count.to_numpy() + 1e-10), 512))
+    gui.show()
